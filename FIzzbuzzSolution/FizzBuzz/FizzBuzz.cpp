1f52b3a2c87c32be43066312eb01e6bb00c6f6aa
GrzegorzAKGalzo <grzeslach8@gmail.com>
2020-11-10 22:30:52 +0100

    FizzBuzz 1.0
    FIzzBuzz Solution only for numbers bigger than 0

---
+++ FIzzbuzzSolution/FizzBuzz/FizzBuzz.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string fizzBuzz(int a) {
+    if (a%3 == 0 && a % 5 ==0) {
+        return "Fizz Buzz";
+    }else if (a % 3 == 0) {
+        return "Fizz";
+    }else if (a % 5 == 0) {
+        return "Buzz";
+    } else {
+        return to_string(a);
+    }
+}
+
+int main()
+{
+    int maxNumber = 0;
+    cin >> maxNumber;
+    if (maxNumber == NULL || maxNumber < 0 || maxNumber == 0) {
+        cout << "Please insert correct number. Bigger than 0!. Tahnk you" << endl;
+    } else {
+        for (int i = 1; i <= maxNumber; i++) {
+            cout << fizzBuzz(i) << ", ";
+        }
+    }
+}
